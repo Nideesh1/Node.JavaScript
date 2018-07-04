382388385224e8defc847c962cabf2ae330b9927
Nideesh1 <nterapalli1@gmail.com>
2018-07-03 18:08:03 -0700

    Add files via upload

---
+++ routes/route.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+router.get('/', function (req, res)  {
+  res.send('<h1>Welcome</h1>')
+  });
+router.get('/about', function (req, res)  {
+  res.send('About Us Page')
+  });
+module.exports = router;
